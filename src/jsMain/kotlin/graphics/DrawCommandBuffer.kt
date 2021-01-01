6fa4e302cca35b5e3b9f08e53d3467799b74f18b
thomasking <ikezaus@gmail.com>
2021-01-01 20:47:43 +1100

    Fixed JS command buffer and draw function, resized default index.html file canvas to 640x480

--- src/jsMain/kotlin/graphics/DrawCommandBuffer.kt
+++ src/jsMain/kotlin/graphics/DrawCommandBuffer.kt
@@ -13,56 +13,74 @@ import org.khronos.webgl.WebGL2RenderingContext.Companion.UNSIGNED_INT
 import kotlin.reflect.KClass
 
 
-actual class DrawCommandBuffer(actual val device: Device, private val buffer: MutableList<() -> Unit>) {
+actual class DrawCommandBuffer(actual val device: Device, private val commands: MutableList<() -> Unit>) {
 
     actual fun bindDataBuffer(index: Int, buffer: Buffer<DataBuffer, *>?) {
-        device.context.bindBufferBase(UNIFORM_BUFFER, index, buffer?.handle)
+        commands.add { device.context.bindBufferBase(UNIFORM_BUFFER, index, buffer?.handle) }
     }
 
     actual fun bindVertexBuffer(index: Int, buffer: Buffer<VertexBuffer, *>?, type: KClass<*>, offset: Long, stride: Int) {
-        when (buffer) {
-            null -> device.context.disableVertexAttribArray(index)
-            else -> device.context.enableVertexAttribArray(index)
+        if (buffer == null) {
+            commands.add { device.context.disableVertexAttribArray(index) }
+            return
         }
-        (buffer ?: return) {
-            when (type) {
-                Float::class -> device.context.vertexAttribPointer(index, 1, FLOAT, false, stride, offset.toInt())
-                Float2::class -> device.context.vertexAttribPointer(index, 2, FLOAT, false, stride, offset.toInt())
-                Float3::class -> device.context.vertexAttribPointer(index, 3, FLOAT, false, stride, offset.toInt())
-                Float4::class -> device.context.vertexAttribPointer(index, 4, FLOAT, false, stride, offset.toInt())
-                Int::class -> device.context.vertexAttribIPointer(index, 1, INT, stride.toLong(), offset)
-                Int2::class -> device.context.vertexAttribIPointer(index, 2, INT, stride.toLong(), offset)
-                Int3::class -> device.context.vertexAttribIPointer(index, 3, INT, stride.toLong(), offset)
-                Int4::class -> device.context.vertexAttribIPointer(index, 4, INT, stride.toLong(), offset)
+
+        commands.add { device.context.enableVertexAttribArray(index) }
+        when (type) {
+            Float::class -> commands.add {
+                buffer { device.context.vertexAttribPointer(index, 1, FLOAT, false, stride, offset.toInt()) }
+            }
+            Float2::class -> commands.add {
+                buffer { device.context.vertexAttribPointer(index, 2, FLOAT, false, stride, offset.toInt()) }
+            }
+            Float3::class -> commands.add {
+                buffer { device.context.vertexAttribPointer(index, 3, FLOAT, false, stride, offset.toInt()) }
+            }
+            Float4::class -> commands.add {
+                buffer { device.context.vertexAttribPointer(index, 4, FLOAT, false, stride, offset.toInt()) }
+            }
+            Int::class -> commands.add {
+                buffer { device.context.vertexAttribIPointer(index, 1, INT, stride.toLong(), offset) }
+            }
+            Int2::class -> commands.add {
+                buffer { device.context.vertexAttribIPointer(index, 2, INT, stride.toLong(), offset) }
+            }
+            Int3::class -> commands.add {
+                buffer { device.context.vertexAttribIPointer(index, 3, INT, stride.toLong(), offset) }
+            }
+            Int4::class -> commands.add {
+                buffer { device.context.vertexAttribIPointer(index, 4, INT, stride.toLong(), offset) }
             }
         }
     }
 
     actual fun bindIndexBuffer(buffer: Buffer<IndexBuffer, *>?) {
-        device.context.bindBuffer(ELEMENT_ARRAY_BUFFER, buffer?.handle)
+        commands.add { device.context.bindBuffer(ELEMENT_ARRAY_BUFFER, buffer?.handle) }
     }
 
     actual fun bindPipeline(pipeline: Pipeline?) {
-        device.context.useProgram(pipeline?.handle)
+        commands.add { device.context.useProgram(pipeline?.handle) }
     }
 
 
     actual fun drawPrimitives(primitiveType: PrimitiveType, vertexCount: Int) {
-        device.context.drawArrays(primitiveType.native, 0, vertexCount)
+        commands.add { device.context.drawArrays(primitiveType.native, 0, vertexCount) }
     }
 
     actual fun drawIndexedPrimitives(primitiveType: PrimitiveType, indexCount: Int) {
-        device.context.drawElements(primitiveType.native, indexCount, UNSIGNED_INT, 0)
+        commands.add { device.context.drawElements(primitiveType.native, indexCount, UNSIGNED_INT, 0) }
     }
 
 
     actual fun clearFramebuffer(color: Color) {
-        device.context.clearColor(color.r, color.g, color.b, color.a)
-        device.context.clear(COLOR_BUFFER_BIT or DEPTH_BUFFER_BIT or STENCIL_BUFFER_BIT)
+        commands.add {
+            device.context.clearColor(color.r, color.g, color.b, color.a)
+            device.context.clear(COLOR_BUFFER_BIT or DEPTH_BUFFER_BIT or STENCIL_BUFFER_BIT)
+        }
     }
 
     actual fun submit() {
-        for (it in buffer) it()
+        for (command in commands) command()
     }
 
 }
